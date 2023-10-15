069a745e182ffbfa428d9d8fb3520e934b04a0a4
Thiago Lopes <thls@cin.ufpe.br>
2023-10-14 23:22:01 -0300

    feat(User): add administrator association to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  # Associations
+  has_one :administrator, dependent: :destroy
+  has_one :manager, dependent: :destroy
+
+  def is_administrator?
+    administrator.present?
+  end
+
 end
