6a6fbe58be3f70801c5a98553cfa8982060a7418
Thiago Lopes <thls@cin.ufpe.br>
2023-10-14 23:27:20 -0300

    chore(schema): update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_15_014135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "administrators", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_administrators_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_15_014135) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "administrators", "users"
 end
