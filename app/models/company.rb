10c1c4aad02f4ae8fe1655f2081c83f0cd460270
Thiago Lopes <thls@cin.ufpe.br>
2023-10-15 09:56:25 -0300

    feat(validation): add validations to Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,9 @@
 class Company < ApplicationRecord
+  # Regular expression for CNPJ validation
+  CNPJ_REGEX = /\A\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}\z/
+
+  # Validate CNPJ format using the regex
+  validates_presence_of :cnpj, :business_name
+  validates_uniqueness_of :cnpj
+  validates_format_of :cnpj, with: CNPJ_REGEX, message: 'is not a valid CNPJ'
 end
