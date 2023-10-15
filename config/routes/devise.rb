612c26f2bb1a781f132d3d13e9f99f31cbba1b9f
Thiago Lopes <thls@cin.ufpe.br>
2023-10-15 07:57:06 -0300

    fix(routes): fix devise path

--- config/routes/devise.rb
+++ config/routes/devise.rb
@@ -1,7 +1,7 @@
 devise_for :users,
   skip: [:registrations, :passwords],
   path: 'auth',
-  path_names: { sign_in: 'redu/sign_in', sign_out: 'redu/sign_out' },
+  path_names: { sign_in: 'sign_in', sign_out: 'sign_out' },
   controllers: {
     sessions: 'auth/sessions'
   }
