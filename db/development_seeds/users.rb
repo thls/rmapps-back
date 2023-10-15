a9c2089bb371555510baca2a92c502318eddcbd4
Thiago Lopes <thls@cin.ufpe.br>
2023-10-15 08:18:17 -0300

    refactor(user_seed): add administrator creation in user seed

--- db/development_seeds/users.rb
+++ db/development_seeds/users.rb
@@ -1,18 +1,36 @@
 # Print a message indicating that the users seed is running
 puts 'Running users seed...'
 
-# Default password for all users
+# Define a method to create a user
+def create_user(user_params)
+  User.create!(user_params)
+end
+
+# Set a default password for all users
 password = '123456'
 
-# Create 20 user records
+# Define user parameters for the administrator user
+admin_user_params = {
+  email: "administrator@example.com",
+  password: password,
+  password_confirmation: password,
+}
+
+# Create the administrator user
+administrator = create_user(admin_user_params)
+
+# Create an Administrator record associated with the administrator user
+Administrator.create!(user: administrator)
+
+# Create 10 common user records with similar parameters
 (1..10).each do |i|
-  # Define user parameters for creating a new user
-  user_params = {
+  # Define common user parameters for creating a new common user
+  common_user_params = {
     email: "user#{i}@example.com",
     password: password,
     password_confirmation: password,
   }
 
-  # Create a new user record with the defined parameters
-  user = User.create!(user_params)
+  # Create a new common user
+  create_user(common_user_params)
 end
