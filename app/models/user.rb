44d05a82f2460c23f081059a53738b7a60a5314b
Thiago Lopes <thls@cin.ufpe.br>
2023-10-15 14:55:16 -0300

    docs(comments): add new comments to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
+  # Include Devise JWT Revocation Strategy (JTIMatcher)
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # Include default Devise modules.
+  # Others available are: :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
   devise :database_authenticatable, :recoverable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   # Associations
   has_one :administrator, dependent: :destroy
   has_one :manager, dependent: :destroy
 
+  # Method to check if the user is an administrator
   def is_administrator?
     administrator.present?
   end
-
 end
